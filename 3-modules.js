efabb6923922a8468afd192e2b7d329d4814913a
jc2545 <[jc2545@cornell.edu]>
2023-05-27 21:55:58 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const {john, peter} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
